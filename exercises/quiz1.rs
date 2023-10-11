a7e37313cee7c8e69b13914e19347b9153f3388b
wiketool <64351271+wiketool@users.noreply.github.com>
2023-10-11 22:09:29 +0800

    40% finish

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(num:i32)->i32{
+    let price = if num > 40 { 1 } else {2};
+    let total  = price * num;
+    return total;
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
